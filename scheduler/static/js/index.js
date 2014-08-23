4210a70df90452cc0375957cf1f4169215130755
Luke Zhao <luke.zhao@polar.me>
2014-08-22 21:04:41 -0400

    fix console error

--- scheduler/static/js/index.js
+++ scheduler/static/js/index.js
@@ -148,7 +148,7 @@ AddCourseModal=React.createClass({
       var matchedSubjects = allSubjects.filter(function(subjectData){
         return subjectData.name.lastIndexOf(subject, 0) === 0
       })
-      if(matchedSubjects[0].name==subject&&catalog_number!=""){
+      if(matchedSubjects.length>0&&matchedSubjects[0].name==subject&&catalog_number!=""){
         state.dataList=matchedSubjects[0].courses.filter(function(courseData){
           return courseData.catalog_number.lastIndexOf(catalog_number, 0) === 0
         })
